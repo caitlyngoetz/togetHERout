98ab127cc471750b97e0d33901eb341185a4c0e0
Caitlyn <caitlyngoetz@u.boisestate.edu>
2016-04-14 18:27:37 -0600

    Adding  these files for the first time

---
+++ header.php
@@ -0,0 +1,20 @@
+<head>
+	<title>togetHERout</title>
+	<meta name="description" content="This is the homepage for the organization togetHERout" />
+	<meta name="keyewords" content="togetHERout, women, exercise, gathering" />
+
+	<div class="banner">
+		<div class="logo">
+			<!--INSERT THE LOGO HERE-->
+		</div>
+	</div>
+
+	<div id="navigation">
+			<ul>
+				<li><a href="homepage.php" class="home">Home</a></li>
+				<li><a href="about.php" class="about">About Us</a></li>
+				<li><a href="calendar.php" class="calendar">Eventures</a></li>
+				<li><a href="blog.php" class="blog">Blog</a></li>
+			</ul>
+		</div>
+</head
---
+++ homepage.php
@@ -0,0 +1,9 @@
+<html>
+	
+	<?php include_once('header.php'); ?>
+	<link href="homepage.css" type="text/css" rel="stylesheet" />
+
+<body>
+	
+</body>
+</html>
